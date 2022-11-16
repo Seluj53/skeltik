1d55c2a96ee1edb003a21f040b6b92a6615e8eb4
Seluj53 <52790189+Seluj53@users.noreply.github.com>
2022-11-16 17:18:13 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 		      // if true, will deploy the app using edge functions
 		      // (https://vercel.com/docs/concepts/functions/edge-functions)
 		      // rather than serverless functions
-		      edge: false,
+		      edge: true,
 
 		      // an array of dependencies that esbuild should treat
 		      // as external when bundling functions
@@ -16,7 +16,7 @@ const config = {
 
 		      // if true, will split your app into multiple functions
 		      // instead of creating a single one for the entire app
-		      split: false
+		      split: true
 		    }),
 	},
 	preprocess: [
